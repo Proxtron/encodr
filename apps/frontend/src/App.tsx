930e24127d5c593d515a06baaaa17d59e595cf11
Proxtron <51761303+Proxtron@users.noreply.github.com>
2026-05-24 09:26:17 -0700

    Swap native video player with Vidstack video player component for resolution switching option

--- apps/frontend/src/App.tsx
+++ apps/frontend/src/App.tsx
@@ -1,40 +1,16 @@
-import { useEffect, useRef } from 'react'
 import styles from "./App.module.css"
-import Hls from 'hls.js';
-
+import '@vidstack/react/player/styles/default/theme.css';
+import '@vidstack/react/player/styles/default/layouts/video.css';
+import { MediaPlayer, MediaProvider } from '@vidstack/react';
+import { defaultLayoutIcons, DefaultVideoLayout } from '@vidstack/react/player/layouts/default';
 
 function App() {
-  const videoRef = useRef<HTMLVideoElement>(null);
-
-  useEffect(() => {
-    if(Hls.isSupported()) {
-      const video = videoRef.current;
-      const hls = new Hls();
-
-      hls.on(Hls.Events.MEDIA_ATTACHED, () => {
-        console.log("video attached to hls.js")
-      });
-
-      hls.on(Hls.Events.MANIFEST_PARSED, (event, data) => {
-        console.log(`manifest loaded, found ${data.levels.length} quality level`);
-      });
-
-      hls.on(Hls.Events.ERROR, function (event, data) {
-          console.log(data);
-      });
-
-      hls.loadSource("http://localhost:3000/videos/master.m3u8");
-      hls.attachMedia(video);
-
-      return () => {
-        hls.destroy();
-      }
-    }
-  }, []);
-
   return (
     <main className={styles.main}>
-      <video className={styles.video} ref={videoRef} controls={true}></video>
+      <MediaPlayer src="http://localhost:3000/videos/master.m3u8">
+        <MediaProvider />
+        <DefaultVideoLayout icons={defaultLayoutIcons} />
+      </MediaPlayer>
     </main>
   )
 }
