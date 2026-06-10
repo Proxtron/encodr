a7e2985e09c5d4a8d4fc71c7189705f680c827cc
Proxtron <51761303+Proxtron@users.noreply.github.com>
2026-06-10 10:46:11 -0700

    Add helper function to extract uuidname from an s3 upload key path

--- apps/backend/src/sqs-consumer.ts
+++ apps/backend/src/sqs-consumer.ts
@@ -4,6 +4,7 @@ import { sqs } from "./config/sqs.js";
 import type { S3Event } from "aws-lambda";
 import { transcodingQueue } from "./queues/transcodeQueue.js";
 import { updateStatus } from "./db/video.js";
+import { extractUuidFromS3UploadPath } from "./services/video.js";
 
 const app = Consumer.create({
     queueUrl: env.SQS_QUEUE_URL,
@@ -17,12 +18,14 @@ const app = Consumer.create({
         for(const record of messageBody.Records) {
             const key = record.s3.object.key;
             if(record.s3.configurationId === "transcode-on-upload") {
-                await transcodingQueue.add("transcode", {
+                const job = await transcodingQueue.add("transcode", {
                     bucket: record.s3.bucket,
                     key
                 });
 
-                await updateStatus(key, "QUEUED");
+                const uuid = extractUuidFromS3UploadPath(key);
+                if(!uuid) return;
+                await updateStatus(uuid, "QUEUED");
             }
         }
         
