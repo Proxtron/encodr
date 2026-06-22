cd7e999e9faba63625d99de6f8a99dae9e35eed1
Proxtron <51761303+Proxtron@users.noreply.github.com>
2026-06-22 11:37:15 -0700

    Update statuses when processing and when ready

--- apps/backend/src/jobs/transcode/handler.ts
+++ apps/backend/src/jobs/transcode/handler.ts
@@ -8,6 +8,7 @@ import { createWriteStream } from "node:fs";
 import { mkdir, rm } from "node:fs/promises";
 import { transcode } from "./ffmpeg.js";
 import { syncOutputToS3 } from "./upload.js";
+import * as Video from "../../db/video.js";
 
 export const handleTranscode: Processor = async (job: Job<TranscodeJobData>) => {
     const { uploadS3Key, uuid } = job.data;
@@ -19,6 +20,11 @@ export const handleTranscode: Processor = async (job: Job<TranscodeJobData>) => 
     if(!s3Stream) {
         throw new Error(`File not retrieved from S3 bucket with key: ${uploadS3Key}`)
     }
+
+    await step(
+        `Updating status of video row to PROCESSING for uuid: ${uuid}`,
+        async () => await Video.updateStatus(uuid, "PROCESSING")
+    );
 
     await step(`download ${uploadS3Key} -> ${tempUploadPath}`, async () => {
         await mkdir(tempUploadDirectory, { recursive: true });
@@ -45,6 +51,10 @@ export const handleUpload: Processor = async (job: Job<UploadJobData>) => {
         async () => await rm("tmp/", {recursive: true})
     );
 
+    await step(
+        `Updating status of video row to READY for uuid: ${uuid}`,
+        async () => await Video.updateStatus(uuid, "READY")
+    )
 }
 
 const step = async <T>(context: string, fn: () => Promise<T>): Promise<T> => {
